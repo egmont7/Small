30c5a0edb7e8a6f4b6ca1e1d2d93a5c9c71531db
Caleb Fangmeier <cfangmeier74@gmail.com>
2015-05-13 11:19:36 -0500

    Adds variable stage length Adds constant(ish, not enforced) register outputs(len>1) Adds a few more test patterns

--- PatternGen/pattern_gen.py
+++ PatternGen/pattern_gen.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python3
 """
 TODO:
-    -Fix VHDL w/ new features!
+    -Fix VHDL w/ new features! (or say "screw it, who needs VHDL?")
 """
 import argparse
+import os
 from datetime import datetime
 from collections import defaultdict
 from collections import namedtuple
 
-Register = namedtuple('Register', ['name', 'is_external'])
-Wire = namedtuple('Wire', ['name', 'rhs'])
-Stage = namedtuple('Stage', ['name', 'reps'])
+Register = namedtuple('Register', ['name', 'is_external', 'size'])
+Wire = namedtuple('Wire', ['name', 'rhs', 'size'])
+Stage = namedtuple('Stage', ['name', 'reps', 'is_variable'])
 
 
 def parse_error(message, line_number, line_content):
@@ -26,10 +27,11 @@ def print_parse_summary(registers, wires, stages, patterns):
         print("   {x.name:10} ext:{x.is_external}".format(x=reg))
     print("Found Wires:")
     for wire in wires:
-        print("   {x.name:10} rhs:{x.rhs}".format(x=wire))
+        print("   {x.name:10} rhs:{x.rhs} size:{x.size}".format(x=wire))
     print("Found Stages:")
     for i, stage in enumerate(stages):
-        fmt = "{i}:   {x.name:10} len: {l:4} reps:{x.reps}"
+        fmt = "{i}:   {x.name:10} len: {l:4} reps:{x.reps} \
+                variable:{x.is_variable}"
         print(fmt.format(i=i, x=stage, l=len(patterns[(registers[0].name,
                                                        stage.name)])))
     print("End Parse Summary")
@@ -56,11 +58,12 @@ def parse(lines):
         if line.startswith("reg"):
             toks = line.split()
             if len(toks) == 3 and toks[1] == 'internal':
-                print('Internal signal defined', toks[2])
-                registers.append(Register(name=toks[2], is_external=False))
+                registers.append(Register(name=toks[2], is_external=False,
+                                          size=1))
                 continue
             elif len(toks) == 2:
-                registers.append(Register(name=toks[1], is_external=True))
+                registers.append(Register(name=toks[1], is_external=True,
+                                          size=1))
                 continue
             else:
                 parse_error("Register definition", i, line_orig)
@@ -68,13 +71,23 @@ def parse(lines):
             toks = line.split(':')
             if len(toks) != 2:
                 parse_error("Wire definition", i, line_orig)
-            wires.append(Wire(name=toks[0].split()[1], rhs=toks[1].strip()))
+            if '\'' in toks[1]:
+                size = int(toks[1].split('\'')[0])
+            else:
+                size = 1
+            name = toks[0].split()[1]
+            rhs = toks[1].strip()
+            wires.append(Wire(name=name, rhs=rhs, size=size))
             continue
         if line.startswith('stage'):
             toks = line.split()
             if len(toks) != 3:
                 parse_error("Stage definition", i, line_orig)
-            stage = Stage(name=toks[1], reps=int(toks[2]))
+            if toks[2] == 'variable':
+                stage = Stage(name=toks[1], reps=-1, is_variable=True)
+            else:
+                stage = Stage(name=toks[1], reps=int(toks[2]),
+                              is_variable=False)
             stages.append(stage)
             current_stage = stage
             continue
@@ -158,8 +171,10 @@ VERILOG_STRINGS = {"header": [
                        "module ***DEV_NAME***",
                        "(",
                        "\tinput clk,"],
+                   "sig_in": "\tinput [15:0]stage_{}_cntr,",
                    "sig_out_reg": "\toutput reg {},",
                    "sig_out_wire": "\toutput {},",
+                   "sig_out_wire_n": "\toutput [{}:0]{},",
                    "res_in":  "\tinput reset_gen",
                    "header_end": [
                        ");",
@@ -170,7 +185,7 @@ VERILOG_STRINGS = {"header": [
                    "wire_def": "assign {}={};",
                    "reg_def": "reg {};",
                    "body_start": [
-                       "always @(posedge clk ) begin",
+                       "always @(posedge clk) begin",
                        "\tif(reset_gen == 1) begin",
                        "\t\tcounter <= 0;",
                        "\t\tstage <= 0;",
@@ -207,11 +222,20 @@ VERILOG_STRINGS = {"header": [
 def make_hdl(dev_name, registers, wires, stages, patterns, hdl_strings):
     hdl = []
     hdl.extend(hdl_strings["header"])
-    for sig in sorted(registers+wires, key=lambda x: x.name):
+    for stage in sorted(stages, key=lambda s: s.name):
+        if stage.is_variable:
+            hdl.append(hdl_strings["sig_in"].format(stage.name))
+    sigs = sorted(registers+wires, key=lambda x: x.name)
+    sigs = sorted(sigs, key=lambda x: x.size != 1)
+    for sig in sigs:
         if isinstance(sig, Register) and sig.is_external:
             hdl.append(hdl_strings["sig_out_reg"].format(sig.name))
         if isinstance(sig, Wire):
-            hdl.append(hdl_strings["sig_out_wire"].format(sig.name))
+            if sig.size == 1:
+                hdl.append(hdl_strings["sig_out_wire"].format(sig.name))
+            else:
+                hdl.append(hdl_strings["sig_out_wire_n"].format(sig.size-1,
+                                                                sig.name))
     hdl.append(hdl_strings["res_in"])
     hdl.extend(hdl_strings["header_end"])
     for reg in registers:
@@ -223,13 +247,13 @@ def make_hdl(dev_name, registers, wires, stages, patterns, hdl_strings):
 
     hdl_inner = []
     for stage_num, stage in enumerate(stages):
-        stage_name, reps = stage
+        stage_name, reps, is_variable= stage
         stage_hdl = []
-        n = len(patterns[(registers[0].name, stage.name)])
+        n = len(patterns[(registers[0].name, stage_name)])
         for i in range(n):
             step_hdl = []
             for reg in registers:
-                pattern = patterns[(reg.name, stage.name)]
+                pattern = patterns[(reg.name, stage_name)]
                 if i < 1 or pattern[i-1] != pattern[i]:
                     step_hdl.append(hdl_strings["assign"].format(reg.name,
                                                                  pattern[i]))
@@ -241,7 +265,11 @@ def make_hdl(dev_name, registers, wires, stages, patterns, hdl_strings):
             hdl_inner.append(hdl_strings["if_stage"].format(stage_num))
             hdl_inner.extend(stage_hdl)
             hdl_inner.append(hdl_strings["body_end1"].format(n-1))
-            hdl_inner.append(hdl_strings["body_end2"].format(reps-1))
+            if is_variable:
+                s = "stage_{}_cntr-1".format(stage_name)
+            else:
+                s = str(reps-1)
+            hdl_inner.append(hdl_strings["body_end2"].format(s))
             hdl_inner.append(hdl_strings["body_end3"].format(len(stages)))
             hdl_inner.extend(hdl_strings["body_end4"])
     hdl.extend([hdl_strings["inner_indent"]+x for x in hdl_inner])
@@ -258,12 +286,13 @@ def main(args):
         lines = infile.readlines()
 
     if args.lang == "vhdl":
+        raise ValueError("vhdl currently broken. Aborting... :(")
         hdl_strings = VHDL_STRINGS
-        extension = ".vhd"
+        extension = "vhd"
         comment = "--"
     else:
         hdl_strings = VERILOG_STRINGS
-        extension = ".v"
+        extension = "v"
         comment = "//"
 
     dev_name, registers, wires, stages, patterns = parse(lines)
@@ -277,8 +306,9 @@ def main(args):
         print("*"*40)
         print("*"*40)
 
-    outfile_name = ".".join(args.input.split('.')[:-1])+extension
-
+    f = os.path.split(args.input)[1]
+    f = '.'.join(f.split('.')[:-1])
+    outfile_name = os.path.join(os.getcwd(), "{}.{}".format(f, extension))
     with open(outfile_name, "w") as outfile:
         outfile.write("{}Autogenerated {} on {}\n\n".format(comment,
                                                             args.lang,
