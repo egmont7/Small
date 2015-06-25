789df3b452234e0cdc5e8f591ce43ec5d738428d
Caleb Fangmeier <cfangmeier74@gmail.com>
2015-06-25 09:50:39 -0500

    Fixes a few issues with make_mapping

--- Make_Mapping/make_mapping.py
+++ Make_Mapping/make_mapping.py
@@ -13,34 +13,45 @@ class Passthrough(dict):
     def __setitem__(self, key, value):
         pass
 
-Mapping = namedtuple("Mapping", ("from_", "to", "map_"))
+    def __contains__(self, key):
+        return True
+
+Mapping = namedtuple("Mapping", ("from_", "to"))
 ARGS = None
 
 
 def parse_file():
     mappings = {}
     with open(ARGS.input, 'r') as infile:
-        curr_map = None
+        curr_from, curr_to, curr_map = None, None, None
         for line in infile.readlines():
             line = line.split('#')[0].strip()
             if not line:
                 continue
             if line[:8].lower() == "mapping:":
                 if curr_map:
-                    mappings[(curr_map.from_,curr_map.to)] = curr_map
-                from_, to = line.split()[1:]
-                curr_map = Mapping(from_=from_, to=to, map_={})
-            elif line[:6].lower() == "alias:":
-                from_, to = line.split()[1:]
-                mappings[(from_,to)] = Mapping(from_=from_, to=to,
-                                               map_=Passthrough())
+                    mappings[Mapping(curr_from, curr_to)] = curr_map
+                curr_from, curr_to = line.split()[1:]
+                curr_map = {}
+            elif line[:12].lower() == "passthrough:":
+                pass_from, pass_to = line.split()[1:]
+                mappings[Mapping(pass_from, pass_to)] = Passthrough()
             else:
-                from_, to = line.split(',')
-                curr_map.map_[from_.strip()] = to.strip()
+                map_from, map_to = line.split(',')
+                curr_map[map_from.strip()] = map_to.strip()
         if curr_map:
-            mappings[(curr_map.from_,curr_map.to)] = curr_map
-
+            mappings[Mapping(curr_from,curr_to)] = curr_map
     return mappings
+
+def gen_rev_mappings(mappings):
+    rev_mappings = {}
+    for mapping, map_ in mappings.items():
+        if type(map_) == Passthrough:
+            rev_map = map_
+        else:
+            rev_map = {item:key for key,item in map_.items()}
+        rev_mappings[Mapping(from_=mapping.to, to=mapping.from_)] = rev_map
+    return rev_mappings
 
 
 def disp_maps(mappings):
@@ -48,13 +59,13 @@ def disp_maps(mappings):
     import webbrowser
     import subprocess
 
-    dot = Digraph("Mappngs!")
+    dot = Digraph("Mappings!")
     nodes = set.union(*(set(mapping)
                         for mapping in mappings.keys()))
     for node in nodes:
         dot.node(node, node)
-    for mapping in mappings.values():
-        dot.edge(mapping.from_, mapping.to)
+    for mapping in mappings.keys():
+        dot.edge(mapping.from_, mapping.to, _attributes={"dir":"both"})
     print(mappings.keys())
 
     dot.format = 'png'
@@ -67,17 +78,25 @@ def main():
     mappings = parse_file()
     if ARGS.plot:
         disp_maps(mappings)
+    mappings.update(gen_rev_mappings(mappings))
 
     if len(ARGS.maps) == 0:
+        print("No mapping path given, exiting...")
         return
     ports = []
     for from_, to in zip(ARGS.maps[:-1], ARGS.maps[1:]):
-        map_ = mappings[(from_, to)].map_
+        map_ = mappings[Mapping(from_, to)]
         if not ports:
             for port in map_.keys():
+                if port.startswith("XNCX"): continue
                 ports.append([port])
         for port in ports:
-            port.append(map_[port[-1]])
+            # print(port[-1], map_.keys())
+            if port[-1].startswith("XNCX") or port[-1] not in map_:
+                port.append("XNCX")
+            else:
+                port.append(map_[port[-1]])
+            # print(port,"\n")
 
     print("Mapping for:")
     print(" ".join(ARGS.maps))
