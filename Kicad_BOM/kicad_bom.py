a7344acb375377b1cd181271254aaa885b107686
Caleb Fangmeier <cfangmeier74@gmail.com>
2015-10-20 11:55:05 -0500

    updates to kicad_bom moving from pickledb to sqlite3, checking old data, and moving css/js to separate files

--- Kicad_BOM/kicad_bom.py
+++ Kicad_BOM/kicad_bom.py
@@ -6,6 +6,7 @@ from os.path import split, join
 from decimal import Decimal
 import argparse
 import urllib
+import sys
 import re
 
 import requests
@@ -20,7 +21,8 @@ DigiPart = namedtuple("DigiPart", ("digikey_part_number",
                                    "misc_data"))
 
 PriceBreak = namedtuple("PriceBreak", ("number", "price_dollars"))
-LOCAL_CACHE_FILE = "/home/caleb/.local/digikey_local_cache.db"
+LOCAL_CACHE_FILE = "/home/caleb/.local/digikey_local_cache.sqlite3"
+CUTOFF_TIME = 3600*3  # Seconds
 
 HEAD = """
 <!DOCTYPE html>
@@ -32,202 +34,57 @@ HEAD = """
   </head>
 """
 
-CSS = """<style>
-#parts {
-    font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
-    width: 100%;
-    border-collapse: collapse;
-}
+CSS = open(join(sys.path[0], "kicad_bom.css"), "r").read()
+JS = open(join(sys.path[0], "kicad_bom.js"), "r").read()
 
-#parts td, #parts th {
-    font-size: 1em;
-    border: 1px solid #98bf21;
-    padding: 3px 7px 2px 7px;
-}
 
-#parts th {
-    font-size: 1.1em;
-    text-align: left;
-    padding-top: 5px;
-    padding-bottom: 4px;
-    background-color: #A7C942;
-    color: #ffffff;
-}
-
-#parts thead {
-    font-size: 1.1em;
-    font-weight: bold;
-}
-
-#parts tr.alt td {
-    color: #000000;
-    background-color: #EAF2D3;
-}
-
-#parts tr.select td{
-    color: #000000;
-    background-color: #99CCFF;
-}
-
-#parts td.num {
-    text-align: right;
-}
-</style>
-"""
-JS = """
-<script>
-function find_price_break(price_breaks, n){
-  price_breaks = price_breaks.split(';');
-  var num_breaks = price_breaks.length
-  for(var i=0; i<num_breaks; i++){
-    var break_high = price_breaks[i].split('@');
-    var break_high_n = Number(break_high[0]);
-    var break_high_price = Number(break_high[1]);
-    if(i == 0){
-      if(n < break_high_n){
-        return Number("NaN");
-      }
-      else{
-        continue;
-      }
-    }
-    var break_low = price_breaks[i-1].split('@');
-    var break_low_n = Number(break_low[0]);
-    var break_low_price = Number(break_low[1]);
-    if(break_low_n<=n && n < break_high_n){
-      return break_low_price;
-    }
-  }
-  var break_highest = price_breaks[num_breaks-1].split('@');
-  var break_highest_price = Number(break_high[1]);
-  return break_highest_price
-}
-
-function update_tottot(){
-  var prices = $('.price').text().split('$');
-  var sum = 0.0;
-  for(var i=0; i<prices.length;i++){
-    sum += Number(prices[i]);
-  }
-  $('#pricetot').text('$'+sum.toFixed(6))
-}
-
-function cleanUp(a) {
-  a.textContent = 'Downloaded';
-//  a.dataset.disabled = true;
-  $(a).attr('data-disabled', true);
-
-  // Need a small delay for the revokeObjectURL to work properly.
-  setTimeout(function() {
-    window.URL.revokeObjectURL(a.href);
-    $(a).remove()
-  }, 1500);
-};
-
-function generateFile(){
-  console.log("generating file");
-  var objs = [];
-  $('tr.datarow').each( function(){
-    var data = $(this).children();
-    var obj = {
-      part_number: $(data[2]).text(),
-      description: $(data[1]).text(),
-      unit_price: $(data[8]).text(),
-      total_price: $(data[9]).text(),
-      num_order: $(data[7]).children()[0].value,
-    };
-    objs.push(obj);
-  });
-//    console.log(JSON.stringify(objs));
-    return JSON.stringify(objs);
-}
-
-function downloadFile(){
-  console.log("preparing file for download");
-  window.URL = window.webkitURL || window.URL;
-
-  var prevLink = $('#outlink');
-  var output = $('#output');
-  if (prevLink) {
-    window.URL.revokeObjectURL(prevLink.href);
-    output.innerHTML = '';
-  }
-
-  var bb = new Blob([generateFile()], {type: "application/json"});
-
-  var a = $('<a id=\\\'outlink\\\'>Download ready</a>');
-  a.attr('download',$('#filename').attr('value'));
-  a.attr('href',window.URL.createObjectURL(bb));
-
-  a.attr('data-downloadurl', ["application/json", a.download, a.href].join(':'));
-  a.draggable = true; // Don't really need, but good practice.
-  a.addClass('dragout');
-
-  output.prepend(a);
-
-  a.on('click', function(e) {
-    if ('disabled' in this.dataset) {
-      return false;
-    }
-
-    cleanUp(this);
-  });
-}
-
-$('input').on('focus', function(){
-  $(this).parent().parent().addClass('select');
-});
-
-$('input').on('blur', function(){
-  $(this).parent().parent().removeClass('select');
-});
-
-$('input').on('keypress', function(event) {
-  if(event.type == 'keypress'){
-    var key = event.charCode || event.keyCode;
-    if(key == 13){ // ie. Return
-      return;
-    }
-  }
-  $(this).css('background-color', '#FFCCFF');
-  });
-
-$('input').on('change keypress', function(event) {
-  if(event.type == 'keypress'){
-    var key = event.charCode || event.keyCode;
-    if(key != 13){ // ie. Not Return
-      return;
-    }
-  }
-  $(this).css('background-color', '#FFFFFF');
-  var n = Number($(this).val());
-  if(Number.isNaN(n)){
-    return;
-  }
-  var id_base = $(this).attr('id').split('_')[0];
-  var priceper_field = $('#'+id_base+'_priceper')
-  var price_field = $('#'+id_base+'_price')
-  var priceper = find_price_break(priceper_field.attr('title'),n);
-  var price = priceper*n;
-  priceper_field.text('$'+priceper.toFixed(6));
-  price_field.text('$'+price.toFixed(6));
-  update_tottot();
-});
-</script>
-"""
 class disk_cache:
     def __init__(self, cache_file):
-        import pickledb
-        self.db = pickledb.load(cache_file, False)
+        import sqlite3
+        self.conn = sqlite3.connect(cache_file)
+        self.curs = self.conn.cursor()
+        self._create_table()
+        # self.db = pickledb.load(cache_file, False)
+
+    def _create_table(self):
+        self.curs.execute("""
+        CREATE TABLE IF NOT EXISTS pages (
+            url TEXT,
+            query_date FLOAT,
+            page_text TEXT
+        ); """)
+
+    def _insert_page(self, url, page_text):
+        from time import time
+        now = time()
+        self.curs.execute("""DELETE FROM pages WHERE url=?;""",
+                          (url,))
+        self.curs.execute("""INSERT INTO pages VALUES (?, ?, ?);""",
+                          (url, now, page_text))
+
+    def _query_page(self, url):
+        self.curs.execute("""
+        SELECT query_date,page_text FROM pages 
+            WHERE url=?;""", (url,))
+        return self.curs.fetchone()
+
+    def _is_too_old(self, old_time):
+        from time import time
+        now = time()
+        print(old_time)
+        print(type(old_time))
+        return (now-old_time) > CUTOFF_TIME
 
     def __call__(self, f):
         @wraps(f)
-        def wrapper(arg : str):
-            data = self.db.get(arg)
-            if not data:
+        def wrapper(arg: str):
+            query = self._query_page(arg)
+            if not query or self._is_too_old(query[0]):
                 data = f(arg)
-                self.db.set(arg, data)
-            self.db.dump()
+                self._insert_page(arg, data)
+            else:
+                data = query[1]
+            self.conn.commit()
             return data
         return wrapper
 
